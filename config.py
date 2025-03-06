03c0765fc5245931da51f397d7983bca61d8dfb5
Ansh Riyal <ansh.riyal@gmail.com>
2025-03-06 09:50:22 -0500

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://grocerEase:pinakisir123@grocer
 DB_NAME = os.getenv("DB_NAME", "chatbot_db")
 COLLECTION_NAME = os.getenv("COLLECTION_NAME", "chat_history")
 
-AI_MODEL_API_KEY = os.getenv("AI_MODEL_API_KEY", "your_api_key_here")
+AI_MODEL_API_KEY = os.getenv("AI_MODEL_API_KEY", "AIzaSyAQoT5M5-Q33BNjJLQG4rBZ4TJS_WwO2Uo")
 AI_MODEL_ENDPOINT = os.getenv("AI_MODEL_ENDPOINT", "https://api.example.com/generate")
